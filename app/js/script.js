b0b0c2e07b4f042472198fefd2bf6628412b874d
TyronNguyen <tyyronnn@gmail.com>
2021-04-08 20:20:11 -0700

    update everything

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+const toggle = document.querySelector('#toggle input');
+
+function clickHandler() {
+  if (this.checked) {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+  } else {
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+  }
+}
+toggle.addEventListener('click', clickHandler);
